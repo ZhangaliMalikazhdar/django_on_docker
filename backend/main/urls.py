dcc4543cf66481a3bd32e6b2b31938503096ede7
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-11-30 03:02:17 +0600

    front complete

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list_excel)
+    path('', views.list_excel, name='all'),
+    path('<pk>', views.sanc, name='sanction')
 ]
--- backend/main/views.py
+++ backend/main/views.py
@@ -4,4 +4,14 @@ from .models import Sanction, Organization
 
 
 def list_excel(request):
-    return render(request, 'list.html')
+    orgs = Organization.objects.all()
+    sancs = Sanction.objects.all()
+    context = {'orgs': orgs, 'sancs': sancs}
+    return render(request, 'list.html', context)
+
+
+def sanc(request, pk):
+    org = Organization.objects.get(pk=pk)
+    sancs = Sanction.objects.filter(name=org.name)
+    context = {'sancs': sancs}
+    return render(request, 'sanction.html', context)
